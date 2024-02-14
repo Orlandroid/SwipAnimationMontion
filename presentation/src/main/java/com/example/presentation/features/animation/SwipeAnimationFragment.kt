3d0695a2435a6f094350f9273493b2cfe2f80b62
Orlandroid <lopez.orlando.gold9@gmail.com>
2024-02-14 15:01:07 -0600

    Add Base of animation

---
+++ presentation/src/main/java/com/example/presentation/features/animation/SwipeAnimationFragment.kt
@@ -0,0 +1,17 @@
+package com.example.presentation.features.animation
+
+import com.example.presentation.R
+import com.example.presentation.base.BaseFragment
+import com.example.presentation.databinding.FragmentSwipeAnimationBinding
+
+
+class SwipeAnimationFragment :
+    BaseFragment<FragmentSwipeAnimationBinding>(R.layout.fragment_swipe_animation) {
+
+
+    override fun setUpUi() {
+
+    }
+
+
+}
